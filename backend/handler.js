c539050b6000dfef48e64f46764eace40603af3a
noahramdial57 <noahramdial57@gmail.com>
2022-10-24 21:22:03 -0400

    added new endpoint

--- backend/handler.js
+++ backend/handler.js
@@ -1,18 +1,9 @@
 'use strict';
 
 module.exports.hello = async (event) => {
-  return {
+  const response = {
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
+    body: JSON.stringify('Hello from Lambda!'),
   };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+  return response;
 };
