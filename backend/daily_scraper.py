9cd6fdd726b930a6dc0ce048af4f72c122177a6b
Morphy Kuffour <morphy.kuffour@uconn.edu>
2022-11-10 19:25:05 -0500

    added code to pull data from dining hall website

--- backend/daily_scraper.py
+++ backend/daily_scraper.py
@@ -6,39 +6,33 @@ import json
 
 def getMeals(event):
 
-    # link_list= ["http://nutritionanalysis.dds.uconn.edu/longmenu.aspx?sName=UCONN+Dining+Services&locationNum=03&locationName=Buckley+Dining+Hall&naFlag=1&WeeksMenus=This+Week%27s+Menus&dtdate=",                    # 10%2f25%2f2022&mealName=Breakfast"
-    #             "http://nutritionanalysis.dds.uconn.edu/longmenu.aspx?sName=UCONN+Dining+Services&locationNum=42&locationName=Gelfenbien+Commons%2c+Halal+%26+Kosher&naFlag=1&WeeksMenus=This+Week%27s+Menus&dtdate=",  # 10%2f27%2f2022&mealName=Breakfast"
-    #             "http://nutritionanalysis.dds.uconn.edu/longmenu.aspx?sName=UCONN+Dining+Services&locationNum=05&locationName=McMahon+Dining+Hall&naFlag=1&WeeksMenus=This+Week%27s+Menus&dtdate=",                    # 10%2f27%2f2022&mealName=Breakfast"
-    #             "http://nutritionanalysis.dds.uconn.edu/longmenu.aspx?sName=UCONN+Dining+Services&locationNum=07&locationName=North+Campus+Dining+Hall&naFlag=1&WeeksMenus=This+Week%27s+Menus&dtdate=",                 # 10%2f27%2f2022&mealName=Breakfast"
-    #             "http://nutritionanalysis.dds.uconn.edu/longmenu.aspx?sName=UCONN+Dining+Services&locationNum=15&locationName=Northwest+Marketplace&naFlag=1&WeeksMenus=This+Week%27s+Menus&dtdate=",                # 10%2f27%2f2022&mealName=Breakfast
-    #             "http://nutritionanalysis.dds.uconn.edu/longmenu.aspx?sName=UCONN+Dining+Services&locationNum=06&locationName=Putnam+Dining+Hall&naFlag=1&WeeksMenus=This+Week%27s+Menus&dtdate=",                      # 10%2f27%2f2022&mealName=Breakfast"
-    #             "http://nutritionanalysis.dds.uconn.edu/longmenu.aspx?sName=UCONN+Dining+Services&locationNum=16&locationName=South+Campus+Marketplace&naFlag=1&WeeksMenus=This+Week%27s+Menus&dtdate=",                 # 10%2f27%2f2022&mealName=Breakfast"
-    #             "http://nutritionanalysis.dds.uconn.edu/longmenu.aspx?sName=UCONN+Dining+Services&locationNum=01&locationName=Whitney+Dining+Hall&naFlag=1&WeeksMenus=This+Week%27s+Menus&dtdate=",
-    #             "http://nutritionanalysis.dds.uconn.edu/longmenu.aspx?sName=UCONN+Dining+Services&locationNum=42&locationName=Gelfenbien+Commons%2c+Halal+%26+Kosher&naFlag=1&WeeksMenus=This+Week%27s+Menus&dtdate="]                    # 10%2f27%2f2022&mealName=Breakfast"
-
-    url_buckley = "http://nutritionanalysis.dds.uconn.edu/longmenu.aspx?sName=UCONN+Dining+Services&locationNum=03&locationName=Buckley+Dining+Hall&naFlag=1&WeeksMenus=This+Week%27s+Menus&dtdate="                    # 10%2f25%2f2022&mealName=Breakfast"
-    url_gelfenbien = "http://nutritionanalysis.dds.uconn.edu/longmenu.aspx?sName=UCONN+Dining+Services&locationNum=42&locationName=Gelfenbien+Commons%2c+Halal+%26+Kosher&naFlag=1&WeeksMenus=This+Week%27s+Menus&dtdate="  # 10%2f27%2f2022&mealName=Breakfast"
-    url_mcmahon = "http://nutritionanalysis.dds.uconn.edu/longmenu.aspx?sName=UCONN+Dining+Services&locationNum=05&locationName=McMahon+Dining+Hall&naFlag=1&WeeksMenus=This+Week%27s+Menus&dtdate="                    # 10%2f27%2f2022&mealName=Breakfast"
-    url_north = "http://nutritionanalysis.dds.uconn.edu/longmenu.aspx?sName=UCONN+Dining+Services&locationNum=07&locationName=North+Campus+Dining+Hall&naFlag=1&WeeksMenus=This+Week%27s+Menus&dtdate="                 # 10%2f27%2f2022&mealName=Breakfast"
-    url_northwest = "http://nutritionanalysis.dds.uconn.edu/longmenu.aspx?sName=UCONN+Dining+Services&locationNum=15&locationName=Northwest+Marketplace&naFlag=1&WeeksMenus=This+Week%27s+Menus&dtdate="                # 10%2f27%2f2022&mealName=Breakfast
-    url_putnum = "http://nutritionanalysis.dds.uconn.edu/longmenu.aspx?sName=UCONN+Dining+Services&locationNum=06&locationName=Putnam+Dining+Hall&naFlag=1&WeeksMenus=This+Week%27s+Menus&dtdate="                      # 10%2f27%2f2022&mealName=Breakfast"
-    url_south = "http://nutritionanalysis.dds.uconn.edu/longmenu.aspx?sName=UCONN+Dining+Services&locationNum=16&locationName=South+Campus+Marketplace&naFlag=1&WeeksMenus=This+Week%27s+Menus&dtdate="                 # 10%2f27%2f2022&mealName=Breakfast"
-    url_whitney = "http://nutritionanalysis.dds.uconn.edu/longmenu.aspx?sName=UCONN+Dining+Services&locationNum=01&locationName=Whitney+Dining+Hall&naFlag=1&WeeksMenus=This+Week%27s+Menus&dtdate="                    # 10%2f27%2f2022&mealName=Breakfast"
-
-    link_list = [url_buckley, url_mcmahon, url_north, url_gelfenbien, url_northwest, url_putnum, url_south, url_whitney]
+    link_list= ["http://nutritionanalysis.dds.uconn.edu/longmenu.aspx?sName=UCONN+Dining+Services&locationNum=03&locationName=Buckley+Dining+Hall&naFlag=1&WeeksMenus=This+Week%27s+Menus&dtdate=",                    # 10%2f25%2f2022&mealName=Breakfast"
+                "http://nutritionanalysis.dds.uconn.edu/longmenu.aspx?sName=UCONN+Dining+Services&locationNum=42&locationName=Gelfenbien+Commons%2c+Halal+%26+Kosher&naFlag=1&WeeksMenus=This+Week%27s+Menus&dtdate=",  # 10%2f27%2f2022&mealName=Breakfast"
+                "http://nutritionanalysis.dds.uconn.edu/longmenu.aspx?sName=UCONN+Dining+Services&locationNum=05&locationName=McMahon+Dining+Hall&naFlag=1&WeeksMenus=This+Week%27s+Menus&dtdate=",                    # 10%2f27%2f2022&mealName=Breakfast"
+                "http://nutritionanalysis.dds.uconn.edu/longmenu.aspx?sName=UCONN+Dining+Services&locationNum=07&locationName=North+Campus+Dining+Hall&naFlag=1&WeeksMenus=This+Week%27s+Menus&dtdate=",                 # 10%2f27%2f2022&mealName=Breakfast"
+                "http://nutritionanalysis.dds.uconn.edu/longmenu.aspx?sName=UCONN+Dining+Services&locationNum=15&locationName=Northwest+Marketplace&naFlag=1&WeeksMenus=This+Week%27s+Menus&dtdate=",                # 10%2f27%2f2022&mealName=Breakfast
+                "http://nutritionanalysis.dds.uconn.edu/longmenu.aspx?sName=UCONN+Dining+Services&locationNum=06&locationName=Putnam+Dining+Hall&naFlag=1&WeeksMenus=This+Week%27s+Menus&dtdate=",                      # 10%2f27%2f2022&mealName=Breakfast"
+                "http://nutritionanalysis.dds.uconn.edu/longmenu.aspx?sName=UCONN+Dining+Services&locationNum=16&locationName=South+Campus+Marketplace&naFlag=1&WeeksMenus=This+Week%27s+Menus&dtdate=",                 # 10%2f27%2f2022&mealName=Breakfast"
+                "http://nutritionanalysis.dds.uconn.edu/longmenu.aspx?sName=UCONN+Dining+Services&locationNum=01&locationName=Whitney+Dining+Hall&naFlag=1&WeeksMenus=This+Week%27s+Menus&dtdate=",
+                "http://nutritionanalysis.dds.uconn.edu/longmenu.aspx?sName=UCONN+Dining+Services&locationNum=42&locationName=Gelfenbien+Commons%2c+Halal+%26+Kosher&naFlag=1&WeeksMenus=This+Week%27s+Menus&dtdate="]                    # 10%2f27%2f2022&mealName=Breakfast"
 
     # parse = event['path'].split("/")
-    parse = event.split("/")  # print(parse)
-    dining_hall = parse[1].capitalize()
-    meal = parse[2].capitalize()
+    parse = event.split("/")  
+    # print(parse)
+    dining_hall = parse[0].capitalize()
+    meal = parse[1].capitalize()
+
+    if dining_hall == "Mcmahon":
+        dining_hall = "McMahon"
 
     # What dining hall is it?
+    url = ""
     for link in link_list:
         if dining_hall in link:
             url = link
             break
 
-    # url = "hello"
+    # print(url)
     # Refactor link | Get todays link
     today = date.today()
     dd = today.strftime("%d")
@@ -69,11 +63,28 @@ def getMeals(event):
 
         calories_val = page_soup.find("td", {"class": "nutfactscaloriesval"})
         serving_size = page_soup.findAll("div", {"class": "nutfactsservsize"})
+        allergen_val = page_soup.find("span", {"class": "labelallergensvalue"})
+        # TODO add tags for allergens
+        # allergen_codes = page_soup.find("span", {"class": "labelwebcodesvalue"})
+
 
         myDict = dict()
         myDict["Food Item"] = menu_item.text
         myDict["Calories"] = calories_val.text
         myDict["Serving Size"] = serving_size[1].text
+        myDict["Allegerns"] = allergen_val.text
+
+
+        # TODO add tags for allergens
+        # for tag in allergen_codes:
+        #     instrument = tag.find('img')
+        #     if instrument:  
+        #         # ...create a new string with the content of 'alt' in the middle if 'tag.text'
+        #         tmp = tag.text[:2] + instrument['alt'] + tag.text[2:]
+        #         myDict["Allegerns Codes"] = allergen_codes.text.append(tmp)
+        #     else:   # if we haven't found an <img> tag we just print 'tag.text'
+        #         print(tag.text)
+
 
         for facts in nut_facts:
 
@@ -148,13 +159,54 @@ def getMeals(event):
 
     return L
 
+# def cachetodaysmeals():
+#     meals = ["breakfast", "lunch", "dinner"]
+#     dhs = ["mcmahon", "south", "north", "buckley", "northwest", "putnam", "whitney", "gelfenbien"]
+#     # today_meals = []
+#     for h in dhs:
+#         for m in meals:
+#             print(h + "/" + m)
+#             # data = getMeals(h + "/" + m)
+#             # today_meals.append(data)
+#             # __import__('pprint').pprint(json.dumps(data))
+#
+#     # TODO convert to a csv file
+#     # return today_meals
+
+
 def main():
-    # getMeals("north/lunch")
-    data = getMeals("/dev/north/lunch")
-    __import__('pprint').pprint(json.dumps(data))
+    data = []
+    d0 = getMeals("mcmahon/breakfast")
+    # d1 = getMeals("mcmahon/lunch")
+    # d2 = getMeals("mcmahon/dinner")
+    # d3 = getMeals("south/breakfast")
+    # d4 = getMeals("south/lunch")
+    # d5 = getMeals("south/dinner")
+    # d6 = getMeals("north/breakfast")
+    # d7 = getMeals("north/lunch")
+    # d8 = getMeals("north/dinner")
+    # d9 = getMeals("buckley/breakfast")
+    # d10 = getMeals("buckley/lunch")
+    # d11 = getMeals("buckley/dinner")
+    # d12 = getMeals("northwest/breakfast")
+    # d13 = getMeals("northwest/lunch")
+    # d14 = getMeals("northwest/dinner")
+    # d15 = getMeals("putnam/breakfast")
+    # d16 = getMeals("putnam/lunch")
+    # d17 = getMeals("putnam/dinner")
+    # d18 = getMeals("whitney/breakfast")
+    # d19 = getMeals("whitney/lunch")
+    # d20 = getMeals("whitney/dinner")
+    # d21 = getMeals("gelfenbien/breakfast")
+    # d22 = getMeals("gelfenbien/lunch")
+    # d23 = getMeals("gelfenbien/dinner")
+
+    data.append(d0)
+    # data.extend((d0, d1, d2, d3, d4, d5, d6, d7, d8, d9, d10, d11, d12, d13, d14, d15, d16, d17, d18, d19, d20, d21, d22, d23))
+
+    # data = cachetodaysmeals()
+    __import__('pprint').pprint(data)
 
 if __name__ == "__main__":
     main()
-
-
-
+    # cachetodaysmeals()
