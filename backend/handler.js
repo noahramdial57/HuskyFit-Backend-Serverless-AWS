09f38adaf6bcfb9e79ad08c7773a9943063aa947
noahramdial57 <noahramdial57@gmail.com>
2022-11-25 22:21:10 -0500

    cleanup

--- backend/handler.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports.hello = async (event) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify('Hello from Morphy and Noah!'),
-  };
-  return response;
-};
