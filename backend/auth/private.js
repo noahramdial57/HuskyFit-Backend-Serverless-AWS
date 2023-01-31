846660dfe099e6c41ca34420947904b28c79d461
noahramdial57 <noahramdial57@gmail.com>
2023-01-31 12:57:56 -0500

    Added Authorizer | Created new resource files

--- backend/auth/private.js
+++ backend/auth/private.js
@@ -1,5 +1,5 @@
 const { sendResponse } = require('./validate')
 
 module.exports.handler = async (event) => {
-    return sendResponse(200, { message: `${event.requestContext.authorizer.claims.name} has been authorized` })
+    return sendResponse(200, { message: `${event.requestContext.authorizer.claims.name} has been successfully logged in!` })
 }
