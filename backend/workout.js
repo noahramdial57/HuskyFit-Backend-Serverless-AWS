c386f7933b7205f626c8de93c272170a74d97816
noahramdial57 <noahramdial57@gmail.com>
2023-02-01 15:33:09 -0500

    Authorizer added to workout endpoint

--- backend/workout.js
+++ backend/workout.js
@@ -16,7 +16,6 @@ exports.handler = async (event) => {
     
     // Name of the workout table
     let Table = "HuskyFit-Workout" // process.env.Workout-Table
-
     
     // Parse out the event body
     let parse = JSON.parse(event.body)
@@ -24,17 +23,19 @@ exports.handler = async (event) => {
     // Get todays date to store workout
     var today = new Date();
     var dd = String(today.getDate()).padStart(2, '0');
-    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); 
     var yyyy = today.getFullYear();
-    
     let date = mm + '/' + dd + '/' + yyyy;
 
     // frontend can add any workout attributes
     // we don't have to hard code it in the backend
-    let UserId = parse.UserID
-    let workout = parse
-    delete workout.UserID
-    let item = [workout]
+
+    // *** Actual User ID
+    let UserId = event.requestContext.authorizer.claims.sub
+
+    // let UserId = parse.UserID // comment this out
+    // delete workout.UserID // comment this out
+    let item = [parse] // workout 
     let method = JSON.parse(JSON.stringify(event.httpMethod))
 
     switch (method) {
@@ -62,8 +63,6 @@ exports.handler = async (event) => {
                 // Workout has been added to existing dataset
                 const data = await dynamo.update(params1).promise();
                 body = JSON.stringify("Workout has been added")
-
-
 
                 return {
                     "isBase64Encoded": false,
@@ -98,8 +97,9 @@ exports.handler = async (event) => {
                 };
             }
 
-            // This Works!!
-        case "POST":
+        // Get all of the users workouts | This Works!!
+        case "GET":
+        // case "POST":
             let params = {
                 TableName: Table,
                 Key: {
@@ -114,6 +114,7 @@ exports.handler = async (event) => {
             } catch (err) {
                 console.log(err)
             }
+
             return {
                 "isBase64Encoded": false,
                 "headers": {
