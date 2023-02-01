01ab36141e9882d7db9184761ec5ee1f16861d99
noahramdial57 <noahramdial57@gmail.com>
2023-02-01 13:02:51 -0500

    Cleanup

--- backend/mock.py
+++ backend/mock.py
@@ -1,9 +1,13 @@
 import json
+# import os
 
 def lambda_handler(event, context):
 
     meal = event['path'].split("/")[2] # ['', 'buckley', 'lunch']
     data = []
+
+    # print(os.listdir(os.curdir))  #files and directories
+
 
     with open('mock_data_{}.json'.format(meal), 'r') as f:
         data = json.dumps(json.load(f))
@@ -16,4 +20,8 @@ def lambda_handler(event, context):
         'body': data
     }
 
-    
+# event = {
+#     "path": "/buckley/breakfast"
+# }
+
+# print(lambda_handler(event, 0)['body'])
